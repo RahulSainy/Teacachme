639e2e158c3d3fff08ebe837f130cefc2246cddf
RahulSainy <kradekidrahul@gmail.com>
2023-02-20 19:45:35 +0530

    commit 000

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
