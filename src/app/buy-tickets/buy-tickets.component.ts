8ed8b61de55426f667885eba6f05ada0b15c6c86
RahulSainy <kradekidrahul@gmail.com>
2023-02-22 14:12:40 +0530

    indext.html techacme spelling change

--- src/app/buy-tickets/buy-tickets.component.ts
+++ src/app/buy-tickets/buy-tickets.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyTicketsComponent implements OnInit {
 
+  name!: string;
+  email!: string;
+  phone!: string;
+  selectedEvents: number[] = [];
+
+  events: {name: string, price: number}[] = [
+    {name: 'Event A', price: 10},
+    {name: 'Event B', price: 20},
+    {name: 'Event C', price: 30},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+ 
+  calculateTotal(): number {
+    let total = 0;
+    for (let i = 0; i < this.selectedEvents.length; i++) {
+      total += this.selectedEvents[i];
+    }
+    return total;
+  }
 
+  onSubmit(): void {
+    // Add code to process the payment here
+  }
+
+  onCheckboxChange(event:any, price: number) {
+    if (event.target.checked) {
+      this.selectedEvents.push(price);
+    } else {
+      let index = this.selectedEvents.indexOf(price);
+      this.selectedEvents.splice(index, 1);
+    }
+  }
 }
